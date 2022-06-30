18311e4335a1fc54751168245d40a65afd9e85f0
Pauline Onyango <plwande21@gmail.com>
2022-06-30 09:11:35 -0700

    0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
-#!/bin/bash
-#include <stdio.h>
-/**
-* main - Write '"Programming is like building a multilingual puzzle'
-*Return: Always 0 (success)
-*/
-int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-return (0);}
+#include <studio.h>
+
+int main(){
+
+}
+
+
+
+
+
+
+
+
