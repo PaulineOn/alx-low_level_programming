a6acbb0abd17aacc18589fc43379c251c8303252
Pauline Onyango <plwande21@gmail.com>
2022-07-04 06:28:45 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 /**
- *main - Prints "with proper grammar, but the outcome is a piece of art,"
+ *main - returns 0
  *
- * Return: Always 0 (Success)
- */
+ * 
+ *
+ *Description - main returns 0
+ *
+ *printf - Prints the string to stdout
+ *
+ * Return: 0
+ * 
+ **/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0)
+	return (0);
 }
