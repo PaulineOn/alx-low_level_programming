38de72b4a69dab04ef670c47921abfb38e9e81b0
Pauline Onyango <plwande21@gmail.com>
2022-07-05 04:57:43 -0700

    1alp

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
  * print_alphabet - prints the english alphabet from a-z.
- * Return: Nothing.
+ * Return: Always 0 (Success
  */
 void print_alphabet(void)
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
+	int n;
+	for (n = 'a'; n <= 'z'; n++)
 	{
-		_putchar(c);
+		_putchar(n);
 	}
 	_putchar('\n');
 }
