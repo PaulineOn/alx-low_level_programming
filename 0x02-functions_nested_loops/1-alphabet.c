7498b2e1424705089cdf8c1d14ced70b5093f7b9
Pauline Onyango <plwande21@gmail.com>
2022-07-05 06:35:27 -0700

    1alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 /**
- *print_alphabet - check the code for Holberton School students.
+ * print_alphabet - print the alphabet in lowercase
  *
- *Return: Always 0.
+ * Return: void
  */
 void print_alphabet(void)
 {
-	char alp = 'a';
-	while (alp <= 'z')
+	int c = 'a';
+	while (c <= 'z')
 	{
-		_putchar (alp);
-		alp++;
+		_putchar (c);
+		c++;
 	}
 	_putchar ('\n');
 }
