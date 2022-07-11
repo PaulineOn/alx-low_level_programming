0242e050adbed7670d965e8058246ac241f2c00c
Pauline Onyango <plwande21@gmail.com>
2022-07-11 02:11:34 -0700

    new fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,13 +4,13 @@
  * @s: the string to be printed in rev
  */
 void print_rev(char *s)
-	int i, n;
-	n = 0;
-	while (s[n] != '\0')
-	n++;
-	for (i = n - 1; i >= 0; i--)
+int i, n;
+n = 0;
+while (s[n] != '\0')
+n++;
+for (i = n - 1; i >= 0; i--)
 {
-	_putchar(s[i]);
+_putchar(s[i]);
 }
 _putchar('\n');
 }
