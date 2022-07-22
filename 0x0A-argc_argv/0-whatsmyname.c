3e43170e4beaf179a1612edae509d8e4fb75bc68
Pauline Onyango <plwande21@gmail.com>
2022-07-22 05:20:58 -0700

    hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,12 +5,8 @@
  * @argv: an array of a command listed
  * Return: 0 for success
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-int i;
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
+printf("%s\n", *argv);
 return (0);
 }
