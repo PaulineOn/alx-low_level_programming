7a9f11dd192799079478e1da80c8b3044f3e2124
Pauline Onyango <plwande21@gmail.com>
2022-08-11 05:36:57 -0700

    hot fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,17 +1,21 @@
 #include "lists.h"
 /**
- * list_len - returns then number of elements in a list.
- * @h: singly linked list.
- * Return: number of elements in the list.
+ * list_len - function that returns the number of elements in a linked list_t
+ *           list.
+ *
+ * @h: This is the sigle list
+ *
+ * Return: The number of elements in a linked list_t list
  */
 size_t list_len(const list_t *h)
 {
-size_t nelem;
-nelem = 0;
-hile (h != NULL)
+int count = 0;
+if (h == NULL)
+return (0);
+while (h != NULL)
 {
+count += 1;
 h = h->next;
-nelem++;
 }
-return (nelem);
+return (count);
 }
